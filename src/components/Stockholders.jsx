5e34586db0caa53a3fd80f12ed76bb82da50f22a
Ian Del Duca <ianmdelduca@gmail.com>
2018-04-05 21:25:20 -0400

    end of day before code review

--- src/components/Stockholders.jsx
+++ src/components/Stockholders.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import '../assets/stylesheets/Stockholders.css';
+import micheal from '../assets/images/micheal.png';
+import marshall from '../assets/images/marshall.png';
+import uparrow from '../assets/images/uparrow.png';
+import seventeen from '../assets/images/seventeen.png';
+import fiftyeight from '../assets/images/fiftyeight.png';
+
 
 
 class Stockholders extends React.Component {
@@ -12,9 +19,67 @@ class Stockholders extends React.Component {
     return (
       <main>
         <Navbar />
-        <div className='construction-container'>
-          Stockholders Letter <p>COMING SOON!</p>
-        </div>
+          <summary className='stockholder-container'>
+            <div id='stockholder-greeting'>
+              To Our Stockholders:
+            </div>
+
+
+
+            <p id='first-paragraph'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <br/>
+            <div className='signature-container'>
+              <div className='signature' id='sig-1'>
+                <img src={micheal}/>
+                <div>Micheal J. Demarco</div><br/>Cheif Executive Officer
+              </div>
+              <div className='signature' id='sig-2'>
+                <img src={marshall}/>
+                <div>Marshall B. Tycher</div><br/>Chairman of Roseland<br/>Residential Trust
+              </div>
+            </div>
+            <div className='sig-date'>April XX, 2018</div>
+          </summary>
+          <img id='seventeen' src={seventeen}/>
+          <img id='fiftyeight' src={fiftyeight}/>
       </main>
     )
   }
@@ -24,3 +89,19 @@ class Stockholders extends React.Component {
 
 
 export default Stockholders
+
+// <div className='stockholder-stats-container' id='stats-1'>
+//   <div className='stat-number'>$58m</div>
+//   <div id='by-2018'>by 2018</div>
+//   <hr></hr>
+//   <div className='stats-title'>Title Here</div>
+//   <div className='stats-copy'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+//   eiusmod tempor incididunt.</div>
+// </div>
+// <div className='stockholder-stats-container' id='stats-2'>
+//   <div className='stat-number'><img src={uparrow}/>17.5%</div>
+//   <hr></hr>
+//   <div className='stats-title'>Title Here</div>
+//   <div className='stats-copy'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+//   eiusmod tempor incididunt.</div>
+// </div>
